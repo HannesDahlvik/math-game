4ce513349c02926a7944cd800e1d01cb4f647185
HannesDahl <hannes.dahlvik@gmail.com>
2019-09-11 14:17:41 +0300

    made a randomizer that puts a number between 0 and 100 into one of the buttons

--- main.js
+++ main.js
@@ -15,17 +15,41 @@ number2.innerHTML = (Math.floor(Math.random() * 100))
 number3.innerHTML = (Math.floor(Math.random() * 100))
 number4.innerHTML = (Math.floor(Math.random() * 100))
 
+number1.addEventListener('click', myFuncion);
+number2.addEventListener('click', myFuncion);
+number3.addEventListener('click', myFuncion);
+number4.addEventListener('click', myFuncion);
+
 let array = [number1, number2, number3, number4];
 
-function questionFunction() {
-    function answer(randomNumber1, randomNumber2) {
-        return randomNumber1 + randomNumber2;
-    }
+let rightCounter = 0;
+let wrongCounter = 0;
 
+function questionFunction() {
     array[Math.floor(Math.random() * 4)].innerHTML = answer(randomNumber1, randomNumber2);
 
     question.innerHTML = randomNumber1 + ' + ' + randomNumber2
-
-    console.log(answer(randomNumber1, randomNumber2));
 }
 questionFunction();
+
+function answer(randomNumber1, randomNumber2) {
+    return randomNumber1 + randomNumber2;
+}
+
+console.log(answer(randomNumber1, randomNumber2));
+
+function myFuncion() {
+    if (number1.innerHTML === answer) {
+        rightCounter += 1;
+    } else if (number2.innerHTML === answer) {
+        rightCounter += 1;
+    } else if (number3.innerHTML === answer) {
+        rightCounter += 1;
+    } else if (number4.innerHTML === answer) {
+        rightCounter += 1;
+    }
+}
+
+setInterval(function () {
+    console.log(rightCounter);
+}, 500);
